5ee6c903d4634c984cb243f0cc4e1db8e891acbf
ThisIsTheWizard <sheikhisthewizard@gmail.com>
2025-12-09 23:36:38 +0600

    Initialize WizardBank project with Maven, Spring Boot, and PostgreSQL setup

---
+++ src/main/java/com/wizardcloud/wizardbank/WizardBankApplication.java
@@ -0,0 +1,11 @@
+package com.wizardcloud.wizardbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizardBankApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WizardBankApplication.class, args);
+    }
+}
---
+++ src/test/java/com/wizardcloud/wizardbank/WizardBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wizardcloud.wizardbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizardBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
